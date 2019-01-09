f53840b54d70145c635c6f805a5b220afbe4c036
陈宇冲 <chenyuchong@hujiang.com>
2019-01-09 11:31:20 +0800

    初始化spring-admin服务端

---
+++ src/main/java/com/chen/SpringAdminApplication.java
@@ -0,0 +1,16 @@
+package com.chen;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminApplication.class, args);
+    }
+
+}
+
